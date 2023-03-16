e17ce104952c30cfeaac565bd7e5defb4505f01e
Ivan Turchin <ivan.turchyn94@gmail.com>
2023-03-16 17:51:23 +0000

    Update TestReverseWords.java

---
+++ src/main/java/ua/foxminded/anagrams/TestReverseWords.java
@@ -0,0 +1,12 @@
+package ua.foxminded.anagrams;
+
+public class TestReverseWords {
+
+	public static void main(String[] args) {
+
+		ReverseWords reverseWords = new ReverseWords();
+		reverseWords.validationInput();
+
+	}
+
+}
